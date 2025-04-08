50ea214f3cbcc84d114305667160325d055666a9
Ayanokoji <braganzajohnpaul@gmail.com>
2025-04-07 23:38:05 -0700

    Plug| Play Go

--- app/page.tsx
+++ app/page.tsx
@@ -62,9 +62,22 @@ export default function Home() {
 
 
         <div className="absolute -bottom-0 left-0 right-0">
-          
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120" className="w-full">
+            <path
+              fill="black"
+              fillOpacity="1"
+              d="M0,64L80,69.3C160,75,320,85,480,80C640,75,800,53,960,48C1120,43,1280,53,1360,58.7L1440,64L1440,120L1360,120C1280,120,1120,120,960,120C800,120,640,120,480,120C320,120,160,120,80,120L0,120Z"
+            ></path>
+          </svg>
         </div>
       </div>
+
+      <div className="container mx-auto px-4 py-8">
+        <div className="grid md:grid-cols-2 gap-8">
+
+
+          </div>
+        </div>
 
       </div>
   );
